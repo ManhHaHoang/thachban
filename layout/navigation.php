1f2ddc91d50716157d0cc1b3ccb028e09ccdf5d9
Manh Ha Hoang <manhha.design@gmail.com>
2021-12-06 14:47:29 +0700

    init html

---
+++ layout/navigation.php
@@ -0,0 +1,268 @@
+<nav class="navigation-wrapper">
+    <ul class="main-menu">
+        <li class="main-menu-item is-mobile gotohome-item">
+            <a href="/">
+                <picture>
+                    <source media="(min-width: 320px)" srcset="/images/logo.svg">
+                    <source src="/images/blank.gif">
+                    <img src="/images/blank.gif" alt="Electic - Thiết bị điện tử, điện lạnh">
+                </picture>
+            </a>
+        </li>
+        <li class="main-menu-item">
+            <a class="menu-item-link" href="#">
+                <div class="menu-icon">
+                    <i class="hg-menu-fridge"></i>
+                </div>
+                <div class="menu-text">Tủ lạnh - Tủ đông - Tủ mát</div>
+            </a>
+
+            <div class="main-submenu-wrapper">
+                <aside>
+                    <div class="submenu-item-box">
+                        <a href="#" class="menuitem-title">TỦ LẠNH</a>
+                        <a href="/tu-lanh-tra-gop-0-phan-tram">Trả góp 0 - 0.5%</a>
+                        <a href="/tu-lanh?g=tu-150-300-lit">Tủ lạnh từ 150 - 300 lít</a>
+                        <a href="/tu-lanh?g=tu-301-400-lit">Tủ lạnh từ 301 - 400 lít</a>
+                        <a href="/tu-lanh?g=tu-lon-side-by-side">Tủ lạnh Side by side</a>
+                        <a href="/tu-lanh?g=mini">Tủ lạnh Mini</a>
+                        <a href="/tu-lanh-panasonic">Panasonic</a>
+                        <a href="/tu-lanh-samsung">Samsung</a>
+                        <a href="/tu-lanh-toshiba">Toshiba</a>
+                        <a href="/tu-lanh-aqua-sanyo">Aqua</a>
+                        <a href="/tu-lanh-lg">LG</a>
+                        <a href="/tu-lanh?p=tu-8tr-12tr">Từ 8 - 12 triệu</a>
+                        <a class="is-mobile" href="/tu-lanh">Tất cả</a>
+                    </div>
+                </aside>
+            </div>
+        </li>
+        <li class="main-menu-item">
+            <a class="menu-item-link" href="#">
+                <div class="menu-icon">
+                    <i class="hg-menu-television"></i>
+                </div>
+                <div class="menu-text">Tivi - Loa - Dàn Karaoke</div>
+            </a>
+
+            <div class="main-submenu-wrapper">
+                <aside>
+                    <div class="submenu-item-box">
+                        <a href="#" class="menuitem-title">MÁY GIẶT</a>
+                        <a href="/may-giat-moi">Đời mới 2021</a>
+                        <a href="/may-giat-toshiba">Toshiba</a>
+                        <a href="/may-giat-lg">LG</a>
+                        <a href="/may-giat-panasonic">Panasonic</a>
+                        <a href="/may-giat-samsung">Samsung</a>
+                        <a href="/may-giat-electrolux">Electrolux</a>
+                        <a href="/may-giat-aqua-sanyo">Aqua</a>
+                        <a href="/may-giat?g=cua-truoc">Máy giặt cửa ngang (cửa trước)</a>
+                        <a href="/may-giat-co-say">Máy giặt sấy</a>
+                        <a href="/may-giat?p=duoi-5-trieu">Giá dưới 5 triệu</a>
+                        <a class="is-mobile" href="/tu-lanh">Tất cả</a>
+                    </div>
+                </aside>
+            </div>
+        </li>
+        <li class="main-menu-item">
+            <a class="menu-item-link" href="#">
+                <div class="menu-icon">
+                    <i class="hg-menu-washer"></i>
+                </div>
+                <div class="menu-text">Máy giặt - Máy nước nóng</div>
+            </a>
+
+            <div class="main-submenu-wrapper">
+                <aside>
+                    <div class="submenu-item-box">
+                        <a href="" class="menuitem-title">MÁY LẠNH</a>
+                        <a href="/may-lanh-tra-gop-0-phan-tram">Trả góp 0 - 0.5%</a>
+                        <a href="/may-lanh-moi">Đời mới 2020</a>
+                        <a href="/may-lanh-panasonic">Panasonic</a>
+                        <a href="/may-lanh-daikin">Daikin</a>
+                        <a href="/may-lanh-samsung">Samsung</a>
+                        <a href="/may-lanh?g=1-hp">Máy lạnh 1 HP</a>
+                        <a href="/may-lanh?g=15-hp">Máy lạnh 1.5 HP</a>
+                        <a href="/may-lanh?g=may-lanh-inverter">Máy lạnh Inverter tiết kiệm điện</a>
+                        <a href="/may-lanh?p=duoi-7-trieu">Máy lạnh dưới 7 triệu</a>
+                        <a class="is-mobile" href="/tu-lanh">Tất cả</a>
+                    </div>
+                </aside>
+            </div>
+        </li>
+        <li class="main-menu-item">
+            <a class="menu-item-link" href="#">
+                <div class="menu-icon">
+                    <i class="hg-menu-phone"></i>
+                </div>
+                <div class="menu-text">Điện thoại - Tablet - Phụ kiện</div>
+            </a>
+
+            <div class="main-submenu-wrapper">
+                <aside>
+                    <div class="submenu-item-box">
+                        <a href="" class="menuitem-title">NỒI CÁC LOẠI</a>
+                        <a href="/noi-com-dien">Nồi cơm điện</a>
+                        <a href="/noi-ap-suat">Nồi áp suất</a>
+                        <a href="/lau-dien">Nồi lẩu điện</a>
+                        <a href="/noi-chien-khong-dau">Nồi chiên không dầu</a>
+                        <a class="is-mobile" href="/tu-lanh">Tất cả</a>
+                    </div>
+                    <div class="submenu-item-box">
+                        <a href="" class="menuitem-title">MÁY XAY - VẮT - ÉP</a>
+                        <a href="/may-xay-sinh-to">Máy xay sinh tố</a>
+                        <a href="/may-xay-sinh-to-may-xay-thit">Máy xay thịt</a>
+                        <a href="/may-lam-sua-hat">Máy làm sữa hạt</a>
+                        <a href="/may-ep-trai-cay">Máy ép trái cây</a>
+                        <a href="/may-vat-cam">Máy vắt cam</a>
+                        <a href="/may-danh-trung">Máy đánh trứng</a>
+                        <a class="is-mobile" href="/tu-lanh">Tất cả</a>
+                    </div>
+                </aside>
+            </div>
+        </li>
+        <li class="main-menu-item">
+            <a class="menu-item-link" href="#">
+                <div class="menu-icon">
+                    <i class="hg-menu-air-conditioner icon-new"></i>
+                </div>
+                <div class="menu-text">Máy lạnh - Máy lọc không khí</div>
+            </a>
+
+            <div class="main-submenu-wrapper">
+                <aside>
+                    <div class="submenu-item-box">
+                        <a href="#" class="menuitem-title">PHÒNG ĂN</a>
+                        <a href="/binh-dung-nuoc">Bình đựng nước</a>
+                        <a href="/binh-ly-giu-nhiet">Bình, ly giữ nhiệt</a>
+                        <a href="/chen-bat">Tô, chén, dĩa</a>
+                        <a href="/muong-dua">Đũa - Muỗng</a>
+                        <a href="/ly-bo-ly">Ly, bộ ly</a>
+                        <a href="/bo-am-tra">Bộ ấm trà</a>
+                        <a class="is-mobile" href="/tu-lanh">Tất cả</a>
+                    </div>
+                    <div class="submenu-item-box">
+                        <a class="menuitem-title">VỆ SINH NHÀ CỬA</a>
+                        <a href="/bo-lau-nha">Bộ lau nhà</a>
+                        <a href="/moc-kep-quan-ao">Kẹp, móc quần áo</a>
+                        <a href="/phu-kien-may-giat">Phụ kiện máy giặt</a>
+                        <a href="/ve-sinh-nha-cua">Vệ sinh nhà cửa</a>
+                        <a class="is-mobile" href="/tu-lanh">Tất cả</a>
+                    </div>
+                </aside>
+            </div>
+        </li>
+        <li class="main-menu-item">
+            <a class="menu-item-link" href="#">
+                <div class="menu-icon">
+                    <i class="hg-menu-chicken-tool"></i>
+                </div>
+                <div class="menu-text">Đồ gia dụng - Quạt</div>
+            </a>
+
+            <div class="main-submenu-wrapper">
+                <aside>
+                    <div class="submenu-item-box">
+                        <a href="#" class="menuitem-title">LỌC NƯỚC</a>
+                        <a href="/may-loc-nuoc?g=tu-dung">Máy lọc nước RO tủ đứng</a>
+                        <a href="/may-loc-nuoc?g=lap-am">Máy lọc nước RO âm tủ</a>
+                        <a href="/may-loc-nuoc?g=de-ban">Máy lọc nước RO để bàn</a>
+                        <a href="/may-loc-nuoc?g=ion-am-tao-kiem">Máy lọc nước ion kiềm</a>
+                        <a href="/loc-nuoc-khong-dien">Lọc nước không điện</a>
+                        <a href="/loi-may-loc-nuoc">Lõi máy lọc nước</a>
+                        <a href="/coc-loc-nuoc">Cốc lọc đầu nguồn</a>
+                        <a href="/but-thu-nuoc">Bút thử nước</a>
+                        <a href="/cay-nuoc-nong-lanh">Cây nước nóng lạnh</a>
+                        <a class="is-mobile" href="/tu-lanh">Tất cả</a>
+                    </div>
+                </aside>
+            </div>
+        </li>
+        <li class="main-menu-item">
+            <a class="menu-item-link" href="#">
+                <div class="menu-icon">
+                    <i class="hg-menu-camera"></i>
+                </div>
+                <div class="menu-text">Máy ảnh - Máy Quay - Phụ kiện</div>
+            </a>
+
+            <div class="main-submenu-wrapper">
+                <aside>
+                    <div class="submenu-item-box">
+                        <a href="#" class="menuitem-title">ĐIỆN THOẠI</a>
+                        <a href="/dien-thoai-apple-iphone">iPhone</a>
+                        <a href="/dien-thoai-samsung">Samsung</a>
+                        <a href="/dien-thoai-oppo">Oppo</a>
+                        <a href="/dien-thoai-xiaomi">Xiaomi</a>
+                        <a href="/dien-thoai?p=tu-2-4-trieu">Điện thoại từ 2 - 4 triệu</a>
+                        <a href="/dien-thoai?p=tu-4-7-trieu">Điện thoại từ 4 - 7 triệu</a>
+                        <a href="/dien-thoai#c=42&amp;r=252,253&amp;pi=0">Điện thoại trên 13 triệu</a>
+                        <a href="/may-doi-tra/dien-thoai">Điện thoại cũ giá rẻ</a>
+                        <a href="/dien-thoai-tra-gop-0-phan-tram">Điện thoại trả góp 0-1%</a>
+                        <a class="is-mobile" href="/tu-lanh">Tất cả</a>
+                    </div>
+                </aside>
+            </div>
+        </li>
+        <li class="main-menu-item">
+            <a class="menu-item-link" href="#">
+                <div class="menu-icon">
+                    <i class="hg-menu-tool"></i>
+                </div>
+                <div class="menu-text">Công cụ - Dụng cụ</div>
+            </a>
+
+            <div class="main-submenu-wrapper">
+                <aside>
+                    <div class="submenu-item-box">
+                        <a href="#" class="menuitem-title">ĐỒNG HỒ THỜI TRANG</a>
+                        <a href="/dong-ho-deo-tay-nam">Đồng hồ nam</a>
+                        <a href="/dong-ho-deo-tay-nu">Đồng hồ nữ</a>
+                        <a href="/dong-ho-deo-tay-casio">Đồng hồ Casio</a>
+                        <a href="/dong-ho-deo-tay-orient">Đồng hồ Orient</a>
+                        <a href="/dong-ho-deo-tay-citizen">Đồng hồ Citizen</a>
+                        <a class="is-mobile" href="/tu-lanh">Tất cả</a>
+                    </div>
+                    <div class="submenu-item-box">
+                        <a href="" class="menuitem-title">ĐỒNG HỒ THÔNG MINH</a>
+                        <a href="/dong-ho-thong-minh-apple">Apple Watch</a>
+                        <a href="/dong-ho-thong-minh-samsung">Samsung</a>
+                        <a href="/dong-ho-thong-minh-huawei">Huawei</a>
+                        <a href="/dong-ho-thong-minh-xiaomi">Xiaomi</a>
+                        <a class="is-mobile" href="/tu-lanh">Tất cả</a>
+                    </div>
+                </aside>
+            </div>
+        </li>
+        <li class="main-menu-item">
+            <a class="menu-item-link" href="#">
+                <div class="menu-icon">
+                    <i class="hg-living-room"></i>
+                </div>
+                <div class="menu-text">Kinh nghiệm hay - mẹo vặt</div>
+            </a>
+
+            <div class="main-submenu-wrapper">
+                <aside>
+                    <div class="submenu-item-box">
+                        <a href="#" class="menuitem-title">Tivi, dàn loa âm thanh</a>
+                        <a href="/may-doi-tra/tivi">Tivi</a>
+                        <a href="/may-doi-tra/dan-loa-dvd">Loa Karaoke</a>
+                        <a href="/may-doi-tra/micro">Micro</a>
+                        <a class="is-mobile" href="/tu-lanh">Tất cả</a>
+                    </div>
+                    <div class="submenu-item-box">
+                        <a href="#" class="menuitem-title">Điện gia dụng</a>
+                        <a href="/may-doi-tra/quat">Quạt</a>
+                        <a href="/may-doi-tra/noi-com-dien">Nồi cơm</a>
+                        <a href="/may-doi-tra/may-xay-sinh-to">Sinh tố/ Xay ép</a>
+                        <a href="/may-doi-tra/bep-tu">Bếp điện</a>
+                        <a href="/may-doi-tra/bep-gas">Bếp gas</a>
+                        <a href="/may-doi-tra/ban-ui">Bàn ủi</a>
+                        <a class="is-mobile" href="/tu-lanh">Tất cả</a>
+                    </div>
+                </aside>
+            </div>
+        </li>
+    </ul>
+</nav>
